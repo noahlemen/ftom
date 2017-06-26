796acf826ef5ac97b7a82121604cab9beb5c552c
Noah Lemen <noah.lemen@refinery29.com>
2017-06-26 18:57:33 -0400

    add typechecking

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 module.exports = function ftom(freq, concertPitch) {
   if (concertPitch === undefined) concertPitch = 440;
+
+  if (typeof freq !== 'number') {
+    throw new TypeError("'ftom' expects its first argument to be a number.")
+  }
+
+  if (typeof concertPitch !== 'number') {
+    throw new TypeError("'ftom' expects its second argument to be a number.")
+  }
+
   return 69 + 12 * Math.log2(freq / concertPitch);
 }
